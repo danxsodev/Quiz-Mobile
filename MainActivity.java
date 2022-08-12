79bcb0dceae05842c4675a98b09ad95481d0e1c1
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-08-12 00:43:57 -0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -38,3 +38,71 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+
+
+public class ActivityQ2 extends AppCompatActivity {
+
+    Button buttonResponderQ2;
+    RadioButton radioButtonPizza, radioButtonChurrasco, radioButtonHamburguer, radioButtonEsfiha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Q2);
+
+        buttonResponderQ2 = findViewById(R.id.buttonResponderQ2);
+        radioButtonPizza = findViewById(R.id.radioButtonPizzaQ2);
+        radioButtonChurrasco = findViewById(R.id.radioButtonChurrascoQ2);
+        radioButtonHamburguer = findViewById(R.id.radioButtonHamburguerQ2);
+        radioButtonEsfiha = findViewById(R.id.radioButtonEsfihaQ2);
+
+        buttonResponderQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioButtonChurrasco.isChecked()==true) {
+                    Toast.makeText(ActivityQ2.this, "Alternativa Correta", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(ActivityQ2.this, "Alternativa Incorreta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
+
+
+
+
+public class ActivityQ3 extends AppCompatActivity {
+
+    Button buttonResponderQ3;
+    RadioButton radioButtonChocolate, radioButtonChurros, radioButtonPave, radioButtonMousse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Q3);
+
+        buttonResponderQ3 = findViewById(R.id.buttonResponderQ3);
+        radioButtonChocolate = findViewById(R.id.radioButtonChocolateQ3);
+        radioButtonChurros = findViewById(R.id.radioButtonChurrosQ3);
+        radioButtonPave = findViewById(R.id.radioButtonPaveQ3);
+        radioButtonMousse = findViewById(R.id.radioButtonMousseQ3);
+
+        buttonResponderQ3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioButtonChocolate.isChecked()==true) {
+                    Toast.makeText(ActivityQ3.this, "Alternativa Correta", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(ActivityQ3.this, "Alternativa Incorreta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
